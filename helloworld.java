c16bd5c2a37aabdcdee001f591e652602caae4d2
hqnguyen4180 <hqnguyen4180@gmail.com>
2014-07-08 21:12:17 -0700

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+/**
+**  Java Program 001: Hello World Application
+**  
+**/
+
+public class HelloWorld
+{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello World!");
+  }
+}
